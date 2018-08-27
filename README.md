4d3398d066c6a1b040de0415b155cd0159b23acf
Unknown <jrady721@gmail.com>
2018-08-27 12:02:04 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-School-meal-api
+# School-meal-api (전국 급식 API)
+
+### 설명
+특정날짜의 특정타입의 급식을 가지고 올 수 있습니다.   
+사용법은 meal.php에 기술해 놓았으니 참고해주세요  
+simple_html_dom을 사용하였습니다
+
+### getMeal(date, type, office, school, level)
+> **날짜 (date)  형식: YYYY.mm.dd**   
+ex 2018.08.16
+
+> **타입 (type)**  
+1: 조식, 2: 중식, 3: 석식
+
+> **교육청 (office)**  
+서울시 교육청 : sen.go.kr  
+경기도 교육청 : goe.go.kr  
+강원도 교육청 : kwe.go.kr  
+전라남도 교육청 : jne.go.kr  
+전라북도 교육청 : jbe.go.kr  
+경상남도 교육청 : gne.go.kr  
+경상북도 교육청 : kbe.go.kr  
+부산광역시 교육청 : pen.go.kr  
+제주자치도 교육청 : jje.go.kr  
+충청남도 교육청 : cne.go.kr  
+충청북도 교육청 : cbe.go.kr  
+광주광역시 교육청 : gen.go.kr  
+울산광역시 교육청 : use.go.kr  
+대전광역시 교육청 : dje.go.kr  
+인천광역시 교육청 : ice.go.kr  
+대구광역시 교육청 : dge.go.kr  
+
+> **학쿄코드 (school)**  
+학교코드 검색: https://www.meatwatch.go.kr/biz/bm/sel/schoolListPopup.do
+
+> **학교분류 (level)**  
+유치원: 1, 초등학교 2, 중학교 3, 고등학교 4
+
+> **예시 (example)**  
+http://jrady721.cafe24.com/api/jmeal/2018.08.16/type/1/office/dge.go.kr/school/D100000282/level/4  
+
+> Result:
+```json
+{  
+    "status": "200",  
+    "type": "아침식사",  
+    "menus": [  
+        "크로크무슈",  
+        "코코볼시리얼",  
+        "우유",  
+        "쇠고기퀴노아죽",  
+        "깍두기"  
+    ]  
+}  
+```
---
+++ meal.php
@@ -0,0 +1,52 @@
+<?php
+// include library
+include 'include/simplehtmldom_1_5/simple_html_dom.php';
+
+// 특정 날짜의 특정 타입의 급식 가져오기
+function getMeal($date = null, $type = null, $office = null, $school = null, $level = null)
+{
+    Header('Content-Type: application/json');
+
+    // check data
+    if ($date === null || $type === null || $office === null || $school === null || $level === null) {
+        return json_encode(array('status' => '400', 'message' => '모든 데이터를 올바르게 입력해주세요.'), JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+    }
+
+    // type setting
+    $mealType = array('아침식사', '점심식사', '저녁식사');
+
+    // create json object
+    $meals = array('status' => '200', 'type' => $mealType[$type - 1]);
+
+    // get data
+    $url = 'http://stu.' . $office . '/sts_sci_md01_001.do?schulCode=' . $school . '&schulCrseScCode=' . $level . '&schMmealScCode=' . $type . '&schYmd=' . $date;
+
+    // parsing
+    $html = file_get_html($url);
+    foreach ($html->find('table tr', 0)->find('th') as $index => $element) {
+        // 요소가 현재 날짜이면.
+        if (preg_replace("/\(.*\)/iU", "", $element->plaintext) == $date) {
+            foreach ($html->find('table tr', 2)->find('td') as $index2 => $element2) {
+                // 현재 날짜에 맞는 메뉴를 가져옴
+                if ($index - 1 === $index2) {
+                    $data = preg_replace("/\(.*\)/iU", "", $element2->plaintext);
+
+                    // create menus
+                    $menus = array();
+                    preg_match_all("|(?<hangul>[가-힣]+)|u", $data, $out);
+                    foreach ($out['hangul'] as $menu) {
+                        array_push($menus, $menu);
+                    }
+                    $meals['menus'] = $menus;
+//                    Header('Content-Type: application/json');
+                    return json_encode($meals, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+                }
+            }
+        }
+    }
+    // error
+    return -1;
+}
+
+// getMeal
+echo getMeal('2018.08.16', '1', 'dge.go.kr', 'D100000282', '4');
